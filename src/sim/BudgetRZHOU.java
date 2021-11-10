a3daf94c6602fa7ebb2b1544017e36cacbe9e155
Rui Zhou <zhourui_1997@126.com>
2021-11-10 02:41:24 +0100

    Try own Budget Strategy
    Naive way of directly setting b_i = r + v / n_slots

--- src/sim/BudgetRZHOU.java
+++ src/sim/BudgetRZHOU.java
@@ -1,4 +1,7 @@
 package sim;
+
+import java.util.ArrayList;
+
 /**
  * Implement your team competition agent here
  * Rename the class by substituting "ABC" by your team's initials,
@@ -14,14 +17,18 @@ public class BudgetRZHOU extends Agent{
 
 	@Override
 	public int initialBid(int reserve) {
-		// TODO Auto-generated method stub
-		return 0;
+		// TODO: find a strategy for the initial bid
+		return this.getValue();
 	}
 
 	@Override
 	public int bid(int t, History history, int reserve) {
-		// TODO Auto-generated method stub
-		return 0;
+		// TODO: find a common bid strategy. An idea might be that first be a BB, ...?
+
+		int numSlots = history.getSlotClicks(t-1).size();
+
+		int retBid = reserve + getValue() / (numSlots);
+		return retBid;
 	}
 
 }
